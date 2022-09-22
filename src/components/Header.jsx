8b0353ef8e9b4e307ea20e6597b035e432d0814e
Nikhila16 <41661526+Nikhila16@users.noreply.github.com>
2022-09-22 19:11:07 +0530

    MyNetFlixList
    It is a NetFlix Series List page created using React and Bootstrap

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = (props) => {
+    return <h3 className="card-name">{props.sname}</h3>
+}
+
+export default Header;
---
+++ src/components/data.jsx
@@ -0,0 +1,88 @@
+const Sdata = [
+    {
+        id : "1",
+        sname : "Lucifer",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.4eU-RAd5HnPATbr6DBwEiQHaEK%26pid%3DApi%26h%3D160&f=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/80057918?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "2",
+        sname : "Boss Baby",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.denofgeek.com%2Fwp-content%2Fuploads%2F2017%2F04%2Fboss_baby_main.jpg%3Fresize%3D768%252C432&f=1&nofb=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/80178943?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "3",
+        sname : "DARK",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp4056410.jpg&f=1&nofb=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/80100172?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "4",
+        sname : "Crash Landing on You",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fdrama%2Fimages%2F2%2F2a%2FCrash_Landing_on_You-tvN-2019-02.jpg%2Frevision%2Flatest%3Fcb%3D20191208111650%26path-prefix%3Des&f=1&nofb=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/81159258?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "5",
+        sname : "Law School",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F6.viki.io%2Fimage%2F90d05f10e16a4c549e9349e50b7e024a.jpeg%3Fs%3D900x600%26e%3Dt&f=1&nofb=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/81413647?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "6",
+        sname : "The King (2019)",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F40%2Fd8%2Ff8%2F40d8f8e1fe47e3629b7b15dbeef0509b.jpg&f=1&nofb=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/81260283?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "7",
+        sname : "Castlevania",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.NQgXsfBnyi9gGmcfe13QkgHaEK%26pid%3DApi&f=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/80095241?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "8",
+        sname : "Baki",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnews.otakukart.com%2Fwp-content%2Fuploads%2F2019%2F06%2FBaki-Season-3.jpg&f=1&nofb=1",
+        title : "Netflix Original Series",
+        links : "https://www.netflix.com/title/80204451?s=a&trkid=13747225&t=cp",
+    },
+    {
+        id : "9",
+        sname : "Chemical Hearts",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmovierecipe.infoversant.com%2Fwp-content%2Fuploads%2FChemical-Hearts-on-Amazon.jpg&f=1&nofb=1",
+        title : "Amazon Original Series",
+        links : "https://app.primevideo.com/detail?gti=amzn1.dv.gti.eeb9f077-ff71-0aec-a89c-61481a01c03f&ref_=atv_dp_share_mv&r=web",
+    },
+    {
+        id : "10",
+        sname : "The Lie",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flivecinemanews.com%2Fwp-content%2Fuploads%2F2020%2F09%2Fthe-lie-trailer-amazon-prime-vid.jpg&f=1&nofb=1",
+        title : "Amazon Original Series",
+        links : "https://app.primevideo.com/detail?gti=amzn1.dv.gti.2cba5b07-58ea-f8df-9755-9786ae75e1af&ref_=atv_dp_share_mv&r=web",
+    },
+    {
+        id : "11",
+        sname : "One Child Nation",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.0hRUdSjVf1Tes0LWQAvCrQHaK9%26pid%3DApi&f=1",
+        title : "Amazon Original Series",
+        links : "https://app.primevideo.com/detail?gti=amzn1.dv.gti.1eb70c80-9671-b01a-f383-190cf5ff1d18&ref_=atv_dp_share_mv&r=web",
+    },
+    {
+        id : "12",
+        sname : "Evil Eye",
+        imgsrc : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Ff%2Ffa%2FEvil_Eye_2020_film_poster.png&f=1&nofb=1",
+        title : "Amazon Original Series",
+        links : "https://app.primevideo.com/detail?gti=amzn1.dv.gti.9eba6ea7-e485-59ff-3f6b-3e121810b106&ref_=atv_dp_share_mv&r=web",
+    }
+]
+
+export default Sdata;
